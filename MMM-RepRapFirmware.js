4a4b03684dab16f657fd1d61fb526f0eace75374
Ari Robinson <ari@arirobinson.ca>
2019-10-10 14:55:01 -0700

    Code and readme added

---
+++ MMM-RepRapFirmware.js
@@ -0,0 +1,177 @@
+Module.register("MMM-RepRapFirmware",{
+    // Default module config.
+	defaults: {
+        host: "",
+        updateInterval: "5000",
+        parameters: ["status", "timeLeft", "percent"],
+        showProgressBar: true,
+	},
+
+	// Override dom generator.
+	getDom: function(value) {
+        var wrapper = document.createElement("div");
+
+        if (result != ""){
+            var tableElement = document.createElement("table");
+            tableElement.style.fontSize = "x-large";
+            tableElement.style.lineHeight = "normal";
+
+            var parameters = this.config.parameters;
+            for (i = 0; i < parameters.length; i++){
+                var name = "";
+                var val = "";
+                switch(parameters[i]){
+                    case "status":
+                        name = "Status";
+                        val = this.convertStatus(result.status);
+                        break;
+                    case "percent":
+                        name = "Print Progress";
+                        val = this.calculatePercent((result.timesLeft.filament), (result.printDuration)) + "%";
+                        break;
+                    case "timeLeft":
+                        name = "Time Left";
+                        val = this.convertTime(result.timesLeft.filament);
+                        break;
+                }
+
+                newRow = document.createElement("tr");
+
+                newCell = newRow.insertCell();
+                newText = document.createTextNode(name);
+                newCell.appendChild(newText);
+                
+                newCell = newRow.insertCell();
+                newText = document.createTextNode(val);
+                newCell.appendChild(newText);
+
+                tableElement.appendChild(newRow);
+            }
+
+            wrapper.appendChild(tableElement);
+
+            if (this.config.progressBar && result.timesLeft.filament > 0){
+                var progress = this.calculatePercent((result.timesLeft.filament), (result.printDuration));
+
+                var progressBar = document.createElement("progress");
+                progressBar.max = 100;
+                progressBar.value = progress;
+
+                wrapper.appendChild(progressBar);
+            }
+        }
+
+		return wrapper;
+    },
+    
+    start: function() {
+        var self = this;
+
+        result = "";
+
+        setInterval(function() {
+            self.getUpdate();
+
+        }, this.config.updateInterval);
+    },
+
+    getUpdate: function(arr){
+        var self = this;
+
+        var xmlhttp = new XMLHttpRequest();
+        var url = "http://" + this.config.host + "/rr_status?type=3";
+
+        xmlhttp.onreadystatechange = function() {
+            if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {                
+                result = JSON.parse(xmlhttp.responseText);
+
+                if (self.hidden){
+                    self.show(1000);
+                }
+            }
+        };
+
+        
+
+
+        xmlhttp.open("GET", url, true);
+        xmlhttp.onerror = function(){
+            if (!self.hidden){
+                self.hide(1000);
+            }
+        }
+        xmlhttp.send();
+
+        this.updateDom();
+    },
+
+    convertStatus: function(stateVal){
+        var state = "";
+
+        switch(stateVal){
+            case "C":
+                state = "Processing";
+                break;
+            case "I":
+                state = "Idle";
+                break;
+            case "B":
+                state = "Busy";
+                break;
+            case "P":
+                state = "Printing";
+                break;
+            case "D":
+                state = "Decelerating";
+                break;
+            case "S":
+                state = "Stopped";
+                break;
+            case "R":
+                state = "Resuming";
+                break;
+            case "H":
+                state = "Halted";
+                break;
+            case "F":
+                state = "Flashing";
+                break;
+            case "T":
+                state = "Tool Change";
+                break;
+        }
+
+        return state;
+    },
+
+    convertTime: function(timeVal){
+        var hours = Math.floor(timeVal / (60*60));
+        var minutes =  Math.floor((timeVal - (hours * 60 * 60)) / 60);
+        var seconds = Math.round((timeVal - (hours * 60 * 60) - (minutes * 60)));
+
+/*        if (minutes < 10){
+            minutes = "0" + minutes;
+        }
+        if (seconds < 10){
+            seconds = "0" + seconds;
+        }*/
+
+        var timeString = "";
+
+        if (hours > 0){
+            timeString += hours + "h ";
+            timeString += minutes + "m ";
+        }
+        else if (minutes > 0) {
+            timeString += minutes + "m ";
+        }
+        timeString += seconds + "s";
+
+        return timeString;
+    },
+
+    calculatePercent: function(timeLeft, printDuration){
+        var percent = (100 * printDuration) / (timeLeft + printDuration);
+        return Math.round(percent);
+    },
+});
--- README.md
+++ README.md
@@ -1,2 +1,45 @@
-# MMM-RepRapFirmware
+# Magic Mirror Module: RepRapFirmware
+
+This module for the [Magic Mirror²](https://github.com/MichMich/MagicMirror) allows you to display live info from your 3D printer running [RepRapFirmware](https://github.com/dc42/RepRapFirmware). This firmware is mainly used on [Duet3D control boards (Duet Wifi & Ethernet)](https://www.duet3d.com/).
  
+## Screenshot
+
+![Screenshot](.github/screenshot.png)
+
+## Installing the module
+
+Navigate to your MagicMirror's `modules` folder:
+```
+cd ~/MagicMirror/modules
+```
+
+Clone this repository:
+```
+git clone https://github.com/arirobinson/MMM-RepRapFirmware
+```
+
+## Using the module
+
+To use this module, add it to the modules array in the `config/config.js` file:
+````javascript
+modules: [
+    {
+        module: "MMM-RepRapFirmware",
+        position: "top_right", //this can be any region
+        header: "3D Printer", //name your printer
+        config: {
+            host: "your_3d_printer_ip_address", //ip address of your printer
+        }
+    },
+]
+````
+
+## Configuration options
+
+The following properties can be configured:
+
+| Option            | Description
+| ----------------- | -----------
+| `host`            | IP address of your printer.<br><br> **Example value:** `192.168.0.5`
+| `updateInterval`  | Time between data updates (in ms).<br><br>**Default value:** `5000`
+| `showProgressBar` | Show the progress bar when print is running.<br><br> **Possible values:** `true` or `false` <br> **Default value:** `true`
