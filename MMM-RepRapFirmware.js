91dc1a3f0839ac204bac2e57c30f6600e39ec1f0
kermit297 <lukaszocypa@gmail.com>
2020-03-29 20:52:53 +0200

    remove getUpdate

--- MMM-RepRapFirmware.js
+++ MMM-RepRapFirmware.js
@@ -62,37 +62,37 @@ Module.register("MMM-RepRapFirmware",{
         result = "";
 
         setInterval(function() {
-            self.getUpdate();
+//            self.getUpdate();
 
         }, this.config.updateInterval);
     },
 
-    getUpdate: function(arr){
-        var self = this;
-
-        var xmlhttp = new XMLHttpRequest();
-        var url = "http://" + this.config.host + "/rr_status?type=3";
-
-        xmlhttp.onreadystatechange = function() {
-            if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {                
-                result = JSON.parse(xmlhttp.responseText);
-
-                if (self.hidden){
-                    self.show(1000);
-                }
-            }
-        };
-
-        xmlhttp.open("GET", url, true);
-        xmlhttp.onerror = function(){
-            if (!self.hidden){
-                self.hide(1000);
-            }
-        }
-        xmlhttp.send();
-
-        this.updateDom();
-    },
+//    getUpdate: function(arr){
+//        var self = this;
+//
+//        var xmlhttp = new XMLHttpRequest();
+//        var url = "http://" + this.config.host + "/rr_status?type=3";
+//
+//        xmlhttp.onreadystatechange = function() {
+//            if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+//                result = JSON.parse(xmlhttp.responseText);
+//
+//                if (self.hidden){
+//                    self.show(1000);
+//                }
+//            }
+//        };
+//
+//        xmlhttp.open("GET", url, true);
+//        xmlhttp.onerror = function(){
+//            if (!self.hidden){
+//                self.hide(1000);
+//            }
+//        }
+//        xmlhttp.send();
+//
+//        this.updateDom();
+//    },
 
     convertTime: function(){
 
